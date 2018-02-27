43282586d8fe32cf256d6061d8648dd160f9e940
Ravindu Udugampola <ravindu365@gmail.com>
2018-02-27 13:40:29 -0800

    Update copyStringBackwards.asm

--- HW2/copyStringBackwards.asm
+++ HW2/copyStringBackwards.asm
@@ -5,15 +5,15 @@ title Copying a String Backwards using Irvine Library
 ;	copying it to target variable in reverse
 ; Date: 02/27/2018
 
-include irvine32.inc								;Include Irvine library
+include irvine32.inc						;Include Irvine library
 
 .data
 	source BYTE "This is the source string",0		; Given variables by question 
-	target BYTE SIZEOF source DUP(0)				; Given variables by question 
+	target BYTE SIZEOF source DUP(0)			; Given variables by question 
 
 .code													
-main proc											; This will copy the Source string to the 						
-													; target string in reverse!
+main proc							; This will copy the Source string to the 						
+								; target string in reverse!
 	mov esi, offset source
 	mov ecx, sizeof source
 
@@ -23,7 +23,7 @@ main proc											; This will copy the Source string to the
 		inc esi
 	loop L1
 									
-	mov edx, offset target							; Show the memory location
+	mov edx, offset target					; Show the memory location
 	mov ecx, sizeof target
 	dec ecx
 	call dumpmem
